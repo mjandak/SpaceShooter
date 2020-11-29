3c78da29867e9eea3be2bb678c0e41dc820440a6
mjandak <mirek.jandak@gmail.com>
2020-11-29 20:52:24 +0100

    parallax backgound

--- Assets/Scripts/SpaceBackground.cs
+++ Assets/Scripts/SpaceBackground.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class SpaceBackground : MonoBehaviour
 {
     public float ScrollSpeed;
+    public float ScrollSpeed2;
+    private Material material;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        material = GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Material material = GetComponent<MeshRenderer>().material;
         Vector2 offset = material.mainTextureOffset;
         offset.y += ScrollSpeed * Time.deltaTime;
         material.mainTextureOffset = offset;
+
+        Vector2 secondTextureOffset = material.GetTextureOffset("_SecondTex") + new Vector2(0, ScrollSpeed2 * Time.deltaTime);
+        material.SetTextureOffset("_SecondTex", secondTextureOffset);
     }
 }
